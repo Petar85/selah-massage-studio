692c10931d26fb80b06db5d38008f2ec956bb678
Peter Pan <petarzivkovic85@gmail.com>
2021-03-16 18:08:26 -0700

    deconstructioning the products object

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ class Products {
         try {
             let result = await fetch("products.json");
             let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const {title, price} = item.fields;
+                const {id} = item.sys
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}
+            })
             return data;
         } catch (error) {
             console.log(error);
