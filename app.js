a3a6539090ec2a55f68e5e3dda98c990036123ce
Peter Pan <petarzivkovic85@gmail.com>
2021-03-18 00:58:46 -0700

    set up cart total and cart items

--- app.js
+++ app.js
@@ -92,6 +92,8 @@ class UI {
             tempTotal += item.price * item.amount;
             itemsTotal += item.amount
         })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
     }
 }
 
